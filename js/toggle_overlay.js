418c7813d4cb550537fbb1d70c61a8a36ac36de5
Barry Gilbert <bgilbert@sharpdotinc.com>
2016-07-26 18:42:55 -0400

    Overlays render

--- js/toggle_overlay.js
+++ js/toggle_overlay.js
@@ -5,51 +5,64 @@
     });
 
     function initOverlayToggle(){
-        if ( !location.hostname.match('local') && !location.hostname.match('sharpd') ) {
-            if ( location.hostname.match('bbox') ) {
-                var $account = $('li.account strong');
-                if ( $account.length == 0 ) {
-                    return;
+        var $account = $('li.account strong');
+
+        //User must be logged in to see the overlay
+        if ( $account.length == 0 ) {
+            return;
+        }
+
+        $.getJSON('/toggle_overlays',function(data) {
+            var path = window.location.pathname.substr(1);
+            var img = '';
+
+            for (var key in data) {
+                if (!data.hasOwnProperty(key)) {
+                    continue;
                 }
-                if ( $account.text() != 'admin' ) {
-                    return;
+
+                var obj = data[key];
+                if (obj['rel_path'] == path) {
+                    img = obj['overlay'];
                 }
-            } else {
+            }
+
+            if (img == '') {
                 return;
             }
-        }
-        var toggleDiv       = '<div id="page-overlay"><!-- x --></div>',
-            toggleBtn       = '<p id="overlay-toggle-btn"><a href="#" class="btn btn-main">Toggle Overlay</a><a href="#" class="btn btn-extra btn-new-window"><i class="fa fa-external-link" title="open in new window"></i></a></p>',
-            $pageWrapper    = $('#page-wrapper'),
-            $pageOverlay    = $('#page-overlay'),
-            $editTabs       = $('.block-workbench,section > .tabs'),
-            $alertBar       = $('.alert-block');
 
-        $pageWrapper.prepend(toggleDiv);
-        $pageWrapper.prepend(toggleBtn);
+            var toggleDiv       = '<div id="page-overlay" style="background-image: url(\'' + img + '\');"><!-- x --></div>',
+                toggleBtn       = '<p id="overlay-toggle-btn"><a href="#" class="btn btn-main">Toggle Overlay</a><a href="#" class="btn btn-extra btn-new-window"><i class="fa fa-external-link" title="open in new window"></i></a></p>',
+                $pageWrapper    = $('#page-wrapper'),
+                $pageOverlay    = $('#page-overlay'),
+                $editTabs       = $('.block-workbench,section > .tabs'),
+                $alertBar       = $('.alert-block');
 
-        $('#overlay-toggle-btn .btn-main').click(function(event){
-            if($('#page-overlay').hasClass('active')){
-                $('#page-overlay').removeClass('active');
-                $editTabs.removeClass('hide');
-                $alertBar.removeClass('hide');
-                $('#overlay-toggle-btn').removeClass('active');
-            }else{
-                $('#page-overlay').addClass('active');
-                $editTabs.addClass('hide');
-                $alertBar.addClass('hide');
-                $('#overlay-toggle-btn').addClass('active');
-            }
-            event.preventDefault();
+            $pageWrapper.prepend(toggleDiv);
+            $pageWrapper.prepend(toggleBtn);
+
+            $('#overlay-toggle-btn .btn-main').click(function(event){
+                if($('#page-overlay').hasClass('active')){
+                    $('#page-overlay').removeClass('active');
+                    $editTabs.removeClass('hide');
+                    $alertBar.removeClass('hide');
+                    $('#overlay-toggle-btn').removeClass('active');
+                }else{
+                    $('#page-overlay').addClass('active');
+                    $editTabs.addClass('hide');
+                    $alertBar.addClass('hide');
+                    $('#overlay-toggle-btn').addClass('active');
+                }
+                event.preventDefault();
+            });
+
+            $('#overlay-toggle-btn .btn-new-window').click(function(event){
+                var imgUrl = $('#page-overlay').css('background-image').slice(5, -2);
+                console.log('img url ', imgUrl);
+                window.open(imgUrl, '_blank');
+                event.preventDefault();
+            });
         });
-
-        $('#overlay-toggle-btn .btn-new-window').click(function(event){
-            var imgUrl = $('#page-overlay').css('background-image').slice(5, -2);
-            console.log('img url ', imgUrl);
-            window.open(imgUrl, '_blank');
-            event.preventDefault();
-        });
-
     }
 
 
